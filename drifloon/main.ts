28211d05082009ae123bcaa37ee6540c4a939ef5
荀徒之 <kalxd@126.com>
2021-04-07 21:06:47 +0800

    typescript基本配置。

---
+++ drifloon/main.ts
@@ -0,0 +1,3 @@
+export * as R from "rambda";
+export * as Most from "most";
+export * as DOM from "@cycle/dom";
--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,27 @@ const path = require("path");
 const pkg = require("./package.json");
 
 const config = {
-	entry: "./main",
+	entry: "./drifloon/main.ts",
 	mode: "production",
 	output: {
 		path: path.resolve("./dist"),
 		filename: `drifloon.${pkg.version}.js`,
 		library: "M",
 		libraryTarget: "umd2"
+	},
+
+	resolve: {
+		extensions: [".ts", ".js"]
+	},
+
+	module: {
+		rules: [
+			{
+				use: "ts-loader",
+				test: /\.ts$/,
+				exclude: /node_modules/
+			}
+		]
 	}
 };
 
